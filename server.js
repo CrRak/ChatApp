50dce28572d1db945841711f6331eefbb9ff25fd
Prakhar Londhe <plondhe@vmware.com>
2020-05-16 12:22:01 +0530

    Added CORS allowance for localhost requests

--- server.js
+++ server.js
@@ -15,6 +15,25 @@ app.use(express.static(__dirname + '/dist/ChatApp'));
 app.use(express.json());
 app.use(express.urlencoded());
 
+// Allow cross origin from localhost
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 // Routers 
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
