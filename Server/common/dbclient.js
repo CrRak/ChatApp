80b006c855b6a302acf8a9abeaa1d789aaff5979
Prakhar Londhe <plondhe@vmware.com>
2020-05-26 10:25:21 +0530

    Added environment variable for mongodb server instance

--- Server/common/dbclient.js
+++ Server/common/dbclient.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false";
+require('dotenv').config()
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri);
 
